78faf931bfe37ffdcf7caf58076afe962a4de88c
hilderonny <hilderonny2014@gmail.com>
2026-08-15 14:40:44 +0200

    Websocket Tests aktualisiert

--- test/server/utils/websocketUtils.test.mjs
+++ test/server/utils/websocketUtils.test.mjs
@@ -2,8 +2,9 @@ import path from 'node:path/posix'
 import fs from 'node:fs'
 import { afterEach, beforeEach, describe, it } from 'node:test'
 import supertest from 'supertest'
-import ExpressApplication from '../../ExpressApplication.mjs'
 import assert from 'node:assert'
+
+import websocketUtils from '../../../utils/websocketUtils.mjs'
 
 function createWebsocketMock() {
     return {
@@ -63,67 +64,48 @@ function prepareMessageToRoom(roomNumber, textMessage) {
 
 describe('Websockets', () => {
 
-    let expressApplication
-
     beforeEach(async () => {
-        const dataPath = './test/data'
-        const fullPath = path.resolve(dataPath)
-        if (fs.existsSync(fullPath)) {
-            fs.rmSync(fullPath, { recursive: true })
-        }
-        expressApplication = new ExpressApplication(
-            dataPath,
-            { '/': './test/html/root' }, // htmlPaths
-            'test_secret', // tokenSecret
-        )
         // Benutzer anlegen
-        await supertest(expressApplication.app).post('/api/register').send({ username: 'testusername', password: 'testpassword' }).expect(200)
+        // await supertest(expressApplication).post('/api/register').send({ username: 'testusername', password: 'testpassword' }).expect(200)
     })
 
-    afterEach(() => {
-        expressApplication.shutDown()
+    it('Bei Verbindung wird eine 0x01-Nachricht mit einer vom Server generierten Wobsocket-Id erhalten', () => {
+        let websocketIdReceived = false
+        const meinWebsocket = createWebsocketMock()
+        meinWebsocket.send = (message) => {
+            assert.ok(message)
+            assert.ok(message.byteLength > 1)
+            const buffer = Buffer.from(message)
+            assert.strictEqual(buffer[0], 0x01)
+            websocketIdReceived = true
+        }
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
+        assert.strictEqual(websocketIdReceived, true)
     })
-
-    // describe('Funktion handleWebsocketConnection()', () => {
-
-    //     it('Bei Verbindung wird eine 0x01-Nachricht mit einer vom Server generierten Wobsocket-Id erhalten', () => {
-    //         let websocketIdReceived = false
-    //         websocketMock.send = (message) => {
-    //             assert.ok(message)
-    //             assert.ok(message.byteLength > 1)
-    //             const buffer = Buffer.from(message)
-    //             assert.strictEqual(buffer[0], 0x01)
-    //             websocketIdReceived = true
-    //         }
-    //         expressApplication.handleWebsocketConnection(websocketMock)
-    //         assert.strictEqual(websocketIdReceived, true)
-    //     })
-
-    // })
     
     it('Erhält keine Nachrichten, die mit 0x10 beginnen.', async () => {
         // Erhalt eigener Nachrichten prüfen
         const meinWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
         meinWebsocket.send = (message) => {
             assert.notStrictEqual(message[0], 0x10)
         }
         // So tun, als ob anderer Websocket eine Nachricht schickt
         const andererWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(andererWebsocket)
+        websocketUtils.handleWebsocketConnection(andererWebsocket)
         andererWebsocket.sendEvent('message', prepareJoinRoomMessage(13n))
     })
     
     it('Erhält keine Nachrichten, die mit 0x20 beginnen.', async () => {
         // Erhalt eigener Nachrichten prüfen
         const meinWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
         meinWebsocket.send = (message) => {
             assert.notStrictEqual(message[0], 0x20)
         }
         // So tun, als ob anderer Websocket eine Nachricht schickt
         const andererWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(andererWebsocket)
+        websocketUtils.handleWebsocketConnection(andererWebsocket)
         andererWebsocket.sendEvent('message', prepareJoinRoomMessage(13n))
         andererWebsocket.sendEvent('message', prepareLeaveRoomMessage(13n))
     })
@@ -131,48 +113,48 @@ describe('Websockets', () => {
     it('Erhält keine Nachrichten, die mit 0x30 beginnen.', async () => {
         // Erhalt eigener Nachrichten prüfen
         const meinWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
         meinWebsocket.send = (message) => {
             console.log(message)
             assert.notStrictEqual(message[0], 0x30)
         }
         // So tun, als ob anderer Websocket eine Nachricht schickt
         const andererWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(andererWebsocket)
+        websocketUtils.handleWebsocketConnection(andererWebsocket)
         andererWebsocket.sendEvent('message', prepareMessageToRoom(13n, 'Hallo Raum'))
     })
     
     it('Erhält keine Nachrichten, die mit 0x40 beginnen.', async () => {
         // Erhalt eigener Nachrichten prüfen
         const meinWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
         meinWebsocket.send = (message) => {
             assert.notStrictEqual(message[0], 0x40)
         }
         // So tun, als ob anderer Websocket eine Nachricht schickt
         const andererWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(andererWebsocket)
+        websocketUtils.handleWebsocketConnection(andererWebsocket)
         andererWebsocket.sendEvent('message', prepareMessageToClient(meinWebsocket.id, 'Hallo Du da'))
     })
     
     it('Wenn anderer Teilnehmer 0x30 Nachricht sendet und man im selben Raum ist, wird 0x31 Nachricht erhalten.', async () => {
         // Erhalt eigener Nachrichten prüfen
         const meinWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
         meinWebsocket.sendEvent('message', prepareJoinRoomMessage(13n)) // Raum betreten
         meinWebsocket.send = (message) => {
             assert.strictEqual(message[0], 0x31)
         }
         // So tun, als ob anderer Websocket eine Nachricht schickt
         const andererWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(andererWebsocket)
+        websocketUtils.handleWebsocketConnection(andererWebsocket)
         andererWebsocket.sendEvent('message', prepareMessageToRoom(13n, 'Hallo Raum'))
     })
     
     it('Wenn anderer Teilnehmer 0x30 Nachricht sendet und man im selben Raum mehrmals angemeldet ist, wird 0x31 Nachricht mehrmals erhalten.', async () => {
         // Erhalt eigener Nachrichten prüfen
         const meinWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
         meinWebsocket.sendEvent('message', prepareJoinRoomMessage(13n)) // Raum betreten
         meinWebsocket.sendEvent('message', prepareJoinRoomMessage(13n)) // Raum nochmal betreten
         let callCount = 0
@@ -182,7 +164,7 @@ describe('Websockets', () => {
         }
         // So tun, als ob anderer Websocket eine Nachricht schickt
         const andererWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(andererWebsocket)
+        websocketUtils.handleWebsocketConnection(andererWebsocket)
         andererWebsocket.sendEvent('message', prepareMessageToRoom(13n, 'Hallo Raum'))
         assert.strictEqual(callCount, 2)
     })
@@ -190,7 +172,7 @@ describe('Websockets', () => {
     it('Wenn anderer Teilnehmer 0x30 Nachricht sendet nachdem man einen Raum verlassen hat, wird keine 0x31 Nachricht erhalten.', async () => {
         // Erhalt eigener Nachrichten prüfen
         const meinWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
         meinWebsocket.sendEvent('message', prepareJoinRoomMessage(13n)) // Raum betreten
         meinWebsocket.sendEvent('message', prepareLeaveRoomMessage(13n)) // Raum verlassen
         meinWebsocket.send = (message) => {
@@ -198,14 +180,14 @@ describe('Websockets', () => {
         }
         // So tun, als ob anderer Websocket eine Nachricht schickt
         const andererWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(andererWebsocket)
+        websocketUtils.handleWebsocketConnection(andererWebsocket)
         andererWebsocket.sendEvent('message', prepareMessageToRoom(13n, 'Hallo Raum'))
     })
     
     it('Wenn anderer Teilnehmer 0x30 Nachricht sendet und man im selben Raum mehrmals angemeldet war und sich einmal abgemeldet hat, wird 0x31 Nachricht nur noch einmal erhalten.', async () => {
         // Erhalt eigener Nachrichten prüfen
         const meinWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
         meinWebsocket.sendEvent('message', prepareJoinRoomMessage(13n)) // Raum betreten
         meinWebsocket.sendEvent('message', prepareJoinRoomMessage(13n)) // Raum nochmal betreten
         meinWebsocket.sendEvent('message', prepareLeaveRoomMessage(13n)) // Raum verlassen
@@ -216,7 +198,7 @@ describe('Websockets', () => {
         }
         // So tun, als ob anderer Websocket eine Nachricht schickt
         const andererWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(andererWebsocket)
+        websocketUtils.handleWebsocketConnection(andererWebsocket)
         andererWebsocket.sendEvent('message', prepareMessageToRoom(13n, 'Hallo Raum'))
         assert.strictEqual(callCount, 1)
     })
@@ -224,35 +206,35 @@ describe('Websockets', () => {
     it('Wenn anderer Teilnehmer 0x30 Nachricht sendet und man nicht im selben Raum ist, wird keine 0x31 Nachricht erhalten.', async () => {
         // Erhalt eigener Nachrichten prüfen
         const meinWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
         meinWebsocket.send = (message) => {
             assert.notStrictEqual(message[0], 0x31)
         }
         // So tun, als ob anderer Websocket eine Nachricht schickt
         const andererWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(andererWebsocket)
+        websocketUtils.handleWebsocketConnection(andererWebsocket)
         andererWebsocket.sendEvent('message', prepareMessageToRoom(13n, 'Hallo Raum'))
     })
     
     it('Wenn anderer Teilnehmer 0x40 Nachricht an dritten Teilnehmer sendet, erhält man selbst keine 0x41 Nachricht.', async () => {
         // Erhalt eigener Nachrichten prüfen
         const meinWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
         meinWebsocket.send = (message) => {
             assert.notStrictEqual(message[0], 0x41)
         }
         // So tun, als ob anderer Websocket eine Nachricht schickt
         const andererWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(andererWebsocket)
+        websocketUtils.handleWebsocketConnection(andererWebsocket)
         const dritterWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(dritterWebsocket)
+        websocketUtils.handleWebsocketConnection(dritterWebsocket)
         andererWebsocket.sendEvent('message', prepareMessageToClient(dritterWebsocket.id, 'Hallo Dritter'))
     })
     
     it('Wenn anderer Teilnehmer 0x40 Nachricht an mich schickt, erhalte ich 0x41 Nachricht.', async () => {
         // Erhalt eigener Nachrichten prüfen
         const meinWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
         meinWebsocket.sendEvent('message', prepareJoinRoomMessage(13n)) // Raum betreten
         let messageReceived = false
         meinWebsocket.send = (message) => {
@@ -261,7 +243,7 @@ describe('Websockets', () => {
         }
         // So tun, als ob anderer Websocket eine Nachricht schickt
         const andererWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(andererWebsocket)
+        websocketUtils.handleWebsocketConnection(andererWebsocket)
         andererWebsocket.sendEvent('message', prepareMessageToClient(meinWebsocket.id, 'Hallo Du da'))
         assert.ok(messageReceived)
     })
@@ -269,7 +251,7 @@ describe('Websockets', () => {
     it('0x01 Nachricht enthält Websocket-ID.', async () => {
         // Vorher ein paar andere Websockets registrieren
         for (let i = 0; i < 15; i++) {
-            expressApplication.handleWebsocketConnection(createWebsocketMock())
+            websocketUtils.handleWebsocketConnection(createWebsocketMock())
         }
         const meinWebsocket = createWebsocketMock()
         let websocketIdReceived = false
@@ -278,34 +260,34 @@ describe('Websockets', () => {
             assert.strictEqual(buffer[0], 0x01)
             const dataView = new DataView(message)
             const senderId = dataView.getBigInt64(1, true)
-            assert.ok(senderId === 15n)
+            assert.ok(senderId === meinWebsocket.id)
             websocketIdReceived = true
         }
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
         assert.strictEqual(websocketIdReceived, true)
     })
     
     it('0x31 Nachricht enthält Sender-Websocket-ID.', async () => {
         // Erhalt eigener Nachrichten prüfen
         const meinWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
+        const andererWebsocket = createWebsocketMock()
+        websocketUtils.handleWebsocketConnection(andererWebsocket)
         meinWebsocket.sendEvent('message', prepareJoinRoomMessage(13n)) // Raum betreten
         meinWebsocket.send = (message) => {
             const arrayBuffer = message.buffer
             const dataView = new DataView(arrayBuffer)
             const senderId = dataView.getBigInt64(1, true)
-            assert.ok(senderId === 1n) // Das muss der zweite registrierte Websocket sein
+            assert.ok(senderId === andererWebsocket.id) // Das muss der zweite registrierte Websocket sein
         }
         // So tun, als ob anderer Websocket eine Nachricht schickt
-        const andererWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(andererWebsocket)
         andererWebsocket.sendEvent('message', prepareMessageToRoom(13n, 'Hallo Raum'))
     })
     
     it('0x31 Nachricht enthält Raum-ID.', async () => {
         // Erhalt eigener Nachrichten prüfen
         const meinWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
         meinWebsocket.sendEvent('message', prepareJoinRoomMessage(13n)) // Raum betreten
         meinWebsocket.send = (message) => {
             const arrayBuffer = message.buffer
@@ -315,14 +297,14 @@ describe('Websockets', () => {
         }
         // So tun, als ob anderer Websocket eine Nachricht schickt
         const andererWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(andererWebsocket)
+        websocketUtils.handleWebsocketConnection(andererWebsocket)
         andererWebsocket.sendEvent('message', prepareMessageToRoom(13n, 'Hallo Raum'))
     })
         
     it('0x31 Nachricht enthält Nachricht.', async () => {
         // Erhalt eigener Nachrichten prüfen
         const meinWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
         meinWebsocket.sendEvent('message', prepareJoinRoomMessage(13n)) // Raum betreten
         meinWebsocket.send = (message) => {
             const arrayBuffer = message.buffer
@@ -331,26 +313,26 @@ describe('Websockets', () => {
         }
         // So tun, als ob anderer Websocket eine Nachricht schickt
         const andererWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(andererWebsocket)
+        websocketUtils.handleWebsocketConnection(andererWebsocket)
         andererWebsocket.sendEvent('message', prepareMessageToRoom(13n, 'Hallo Raum'))
     })
     
     it('0x41 Nachricht enthält Sender-Websocket-ID.', async () => {
         // Erhalt eigener Nachrichten prüfen
         const meinWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
+        const andererWebsocket = createWebsocketMock()
+        websocketUtils.handleWebsocketConnection(andererWebsocket)
         meinWebsocket.sendEvent('message', prepareJoinRoomMessage(13n)) // Raum betreten
         let messageReceived = false
         meinWebsocket.send = (message) => {
             const arrayBuffer = message.buffer
             const dataView = new DataView(arrayBuffer)
             const senderId = dataView.getBigInt64(1, true)
-            assert.ok(senderId === 1n) // Das muss der zweite registrierte Websocket sein
+            assert.ok(senderId === andererWebsocket.id) // Das muss der zweite registrierte Websocket sein
             messageReceived = true
         }
         // So tun, als ob anderer Websocket eine Nachricht schickt
-        const andererWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(andererWebsocket)
         andererWebsocket.sendEvent('message', prepareMessageToClient(meinWebsocket.id, 'Hallo Du da'))
         assert.ok(messageReceived)
     })
@@ -358,7 +340,7 @@ describe('Websockets', () => {
     it('0x41 Nachricht enthält Nachricht.', async () => {
         // Erhalt eigener Nachrichten prüfen
         const meinWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(meinWebsocket)
+        websocketUtils.handleWebsocketConnection(meinWebsocket)
         meinWebsocket.sendEvent('message', prepareJoinRoomMessage(13n)) // Raum betreten
         let messageReceived = false
         meinWebsocket.send = (message) => {
@@ -369,7 +351,7 @@ describe('Websockets', () => {
         }
         // So tun, als ob anderer Websocket eine Nachricht schickt
         const andererWebsocket = createWebsocketMock()
-        expressApplication.handleWebsocketConnection(andererWebsocket)
+        websocketUtils.handleWebsocketConnection(andererWebsocket)
         andererWebsocket.sendEvent('message', prepareMessageToClient(meinWebsocket.id, 'Hallo Du da'))
         assert.ok(messageReceived)
     })
